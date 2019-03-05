59d7e77cabab7f7dc6a82a01c37166f774366705
Gravitas <support@designrem.com>
2019-03-05 21:06:52 +0000

    Update.

--- README.md
+++ README.md
@@ -9,32 +9,32 @@ https://www.reddit.com/r/Biohackers/comments/a1vdl7/best_full_body_redlight/
 
 ## Instructions
 
-In a full-body red light therapy pod from www.NovoThor.com, one lies in the pod and receives 16mW/cm2 for 8 minutes, for a total of 8 joules per square centimetre of skin.
+In a full-body red light therapy pod from www.NovoThor.com, one lies in the pod and receives 16mW/cm2 or 1 joule per minute. After 10 minutes, this is 10 joules per square centimetre of skin.
 
 To get the same effect from off-the-shelf grow lights, purchase some panels with the same wavelength as the NovoThor, which is 650nm and 810nm. Arrange four panels on the wall in a 2x2 grid. Using a light meter such as the TES-1333, find the right distance where the light intensity reads "160" on the meter. The meter is in W/m2, so this represents 16mW/cm2 of light intensity, same as the NovoThor.
 
-There are 360 degrees in a circle. Stand facing the panel, then rotate 45 degrees every 60 seconds. After 480 seconds, every square centimetre of skin will have received a fraction of the dose that an 8-minute session in the NovoThor would deliver.
+There are 360 degrees in a circle. Stand facing the panel, then rotate 45 degrees every 60 seconds. After 480 seconds, every square centimetre of skin will have received a fraction of the dose that an 12-minute session in the NovoThor would deliver.
 
 But what is that fraction? Use the attached Python code to get a better estimate.
 
 The output of the program is as follows:
 
 ```c
-Red Light Therapy Dose Calculator
-  - See: https://github.com/sharpe5/red-light-therapy/
+Photobiomodulation (Red Light Therapy) Dose Calculator
+  - Instructions: https://github.com/sharpe5/red-light-therapy/
 User set variables:
   - What does our TES-1333 light meter read in W/m2? 160
-  - Total seconds at each 45 degree angle: 60
+  - Total seconds spent standing at each 45 degree angle: 60
 Calculated (intermediate):
   - Total number of 45 degree angles in 360 degrees: 8
   - Total seconds to rotate 360 degrees: 480
   - Total minutes to rotate 360 degrees: 8.0
   - Total joules/centimetre2 this: 2.3176450198781713
-  - Total minutes in NovoThor: 0.2
+  - Total minutes in NovoThor: 10
   - Light intensity in NovoThor [mW/cm2]: 16.666
-  - Total joules/centimetre2 NovoThor: 11.99952
+  - Total joules/centimetre2 NovoThor: 9.999600000000001
 Calculated (final value):
-  - Percent dose compared to one full-body NovoThor session of 12 minutes @ 16.666 mW/cm2: 19.3%
+  - Percent dose compared to one full-body NovoThor session of 10 minutes @ 16.666 mW/cm2: 23.2%
 ```
 
 ## Summary
--- red-light-therapy-dose-calculator.py
+++ red-light-therapy-dose-calculator.py
@@ -7,46 +7,46 @@ print("  - Instructions: https://github.com/sharpe5/red-light-therapy/")
 print("")
 print("User set variables:")
 
-watts_per_meter = 160
+this_watts_per_meter = 160
 # Note: This is 10x the value in mW/cm2 (milliwatts to watts is 1000, and 10000 square centimetres in a in metre.
-print("  - What does our TES-1333 light meter read in W/m2? " + str(watts_per_meter))
+print("  - What does our TES-1333 light meter read in W/m2? " + str(this_watts_per_meter))
 
 # How many seconds we spend at each 45 degree angle. This is the *only* variable we have to manually change.
-total_seconds_per_eighth = 60
-print("  - Total seconds spent standing at each 45 degree angle: " + str(total_seconds_per_eighth))
+this_total_seconds_per_eighth = 60
+print("  - Total seconds spent standing at each 45 degree angle: " + str(this_total_seconds_per_eighth))
 
 print("")
 print("Calculated (intermediate):")
-total_seconds = total_seconds_per_eighth * 8
+this_total_seconds = this_total_seconds_per_eighth * 8
 print("  - Total number of 45 degree angles in 360 degrees: 8")
 
-print("  - Total seconds to rotate 360 degrees: " + str(total_seconds))
-print("  - Total minutes to rotate 360 degrees: " + str(total_seconds/60))
+print("  - Total seconds to rotate 360 degrees: " + str(this_total_seconds))
+print("  - Total minutes to rotate 360 degrees: " + str(this_total_seconds / 60))
 
-milliwatts_per_centimeter = watts_per_meter / 10
+this_milliwatts_per_centimeter = this_watts_per_meter / 10
 # each square centimetre of skin gets three exposures: facing panel, previous (45 degrees less), next (45 degrees more)
 # first position, facing straight at the panel
-total_joules_position_1 = total_seconds_per_eighth * milliwatts_per_centimeter / 1000
+this_total_joules_position_1 = this_total_seconds_per_eighth * this_milliwatts_per_centimeter / 1000
 # next position, rotated 45 more degrees facing the panel
-total_joules_position_2 = total_joules_position_1 * math.cos(math.radians(45))
+this_total_joules_position_2 = this_total_joules_position_1 * math.cos(math.radians(45))
 # previous position, rotated 45 less degrees facing the panel
-total_joules_position_3 = total_joules_position_1 * math.cos(math.radians(45))
-total_joules_this = total_joules_position_1 + total_joules_position_2 + total_joules_position_3
+this_total_joules_position_3 = this_total_joules_position_1 * math.cos(math.radians(45))
+this_total_joules_this = this_total_joules_position_1 + this_total_joules_position_2 + this_total_joules_position_3
 
-print("  - Total joules/centimetre2 this: " + str(total_joules_this))
+print("  - Total joules/centimetre2 this: " + str(this_total_joules_this))
 
 # What we would get from a full body light pod from www.NovoThor.com in N minutes.
-time_minutes = 12
-print("  - Total minutes in NovoThor: " + str(time_minutes/60))
-time_seconds = 60*time_minutes
-millijoules_per_centimetre = 16.666
-print("  - Light intensity in NovoThor [mW/cm2]: " + str(millijoules_per_centimetre))
-total_joules_novothor = time_seconds * millijoules_per_centimetre / 1000
-print("  - Total joules/centimetre2 NovoThor: " + str(total_joules_novothor))
+novothor_time_minutes = 10
+print("  - Total minutes in NovoThor: " + str(novothor_time_minutes))
+novothor_time_seconds = 60 * novothor_time_minutes
+novothor_millijoules_per_centimetre = 16.666
+print("  - Light intensity in NovoThor [mW/cm2]: " + str(novothor_millijoules_per_centimetre))
+novothor_total_joules_per_square_centimetre = novothor_time_seconds * novothor_millijoules_per_centimetre / 1000
+print("  - Total joules/centimetre2 NovoThor: " + str(novothor_total_joules_per_square_centimetre))
 
-percent_this_of_novothor = total_joules_this / total_joules_novothor * 100
+percent_our_setup_of_novothor = this_total_joules_this / novothor_total_joules_per_square_centimetre * 100
 
 print("")
 print("Calculated (final value):")
-print("  - Percent dose compared to one full-body NovoThor session of " + str(time_minutes) + " minutes @ "
-      + str(millijoules_per_centimetre) + " mW/cm2: " + str(round(percent_this_of_novothor, 1)) + "%")
+print("  - Percent dose compared to one full-body NovoThor session of " + str(novothor_time_minutes) + " minutes @ "
+      + str(novothor_millijoules_per_centimetre) + " mW/cm2: " + str(round(percent_our_setup_of_novothor, 1)) + "%")
