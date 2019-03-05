460b0de94b7dde205f3b1bf0570c4f1afd7f4c54
Gravitas <support@designrem.com>
2019-03-05 22:32:03 +0000

    Update.

--- red-light-therapy-dose-calculator.py
+++ red-light-therapy-dose-calculator.py
@@ -1,5 +1,5 @@
 # Dose calculator for red light therapy (i.e. photobiomodulation).
-# Compares dose from off-the-shell red light therapy panels to dose from a fully body light pod from www.NovoThor.com.
+# Compares dose from off-the-shell red light therapy panels to dose from a full body light pod from www.NovoThor.com.
 # https://github.com/sharpe5/red-light-therapy/
 
 import os
